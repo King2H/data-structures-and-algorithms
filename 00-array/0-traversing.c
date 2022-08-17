ef853fdb6a1aede2a93893646206ea97a61c854d
King2H <neg2htt@gmail.com>
2022-08-17 17:15:44 +0000

    Array element traversing/Printing

--- 00-array/0-traversing.c
+++ 00-array/0-traversing.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	int LA[] = {1,3,5,7,8};
-	int i=0, n=5;
+	int Arr[] = {1,3,5,7,8};
+	int i=0; /* Array element counter */
+	int n=5; /* Array length */
 	printf("The original array elements are:\n");
 	for(i=0; i<n; i++) {
-		printf("LA[%d] = %d \n", i, LA[i]);
+		printf("Arr[%d] = %d \n", i, Arr[i]);
 	}
 	return (0);
 }
